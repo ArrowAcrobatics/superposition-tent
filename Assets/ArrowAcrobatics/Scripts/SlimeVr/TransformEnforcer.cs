bedfb23a723565416b03d004fde16a031fd35cdf
ArrowAcrobatics <arrowacrobatics@gmail.com>
2022-07-18 19:43:04 +0200

    set target enforcer and anchor for main camera and Hips in avatar

--- Assets/ArrowAcrobatics/Scripts/SlimeVr/TransformEnforcer.cs
+++ Assets/ArrowAcrobatics/Scripts/SlimeVr/TransformEnforcer.cs
@@ -8,12 +8,27 @@ public class TransformEnforcer : MonoBehaviour
     public bool setPosition = true;
     public bool setRotation = true;
 
+    /**
+     * If this is set to true, the position of this game component will be achieved by
+     * setting the position of the youngest ancestor with a TransformEnforcerAnchor component
+     * or root if that isn't found. 
+     * 
+     * Intermediate tranforms are left untouched.
+     */
     public bool propagateToParents = true;
 
     void FixedUpdate() {
+        Transform anchor = targetTransform;
+        if(propagateToParents) {
+            while(anchor.GetComponent<TransformEnforcerAnchor>() == null && anchor.parent != null) {
+                anchor = anchor.parent;
+            }
+        }
+
         // update targetTransform
         if(setPosition) {
-            targetTransform.position = transform.position;
+            anchor.position = anchor.position - targetTransform.position + transform.position;
+            //targetTransform.position = transform.position;
         }
         if(setRotation) {
             targetTransform.rotation = transform.rotation;
---
+++ Assets/ArrowAcrobatics/Scripts/SlimeVr/TransformEnforcerAnchor.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Tag behaviour that indicates that transformEnforcer should not
+ * propagate to older ancestors than the transform this component is attached to.
+ */
+public class TransformEnforcerAnchor : MonoBehaviour
+{
+}
